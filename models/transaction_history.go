e0fed4f94f9a0dc87107371fc662c9ffd30bb680
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-05-30 21:55:03 +0700

    Add User attribute to txhistory model

--- models/transaction_history.go
+++ models/transaction_history.go
@@ -7,6 +7,7 @@ type TransactionHistory struct {
 	ProductId  uint `json:"product_id"`
 	Product    Product
 	UserId     uint `json:"user_id"`
+	User       User
 	Quantity   uint `gorm:"not null" json:"quantity"`
 	TotalPrice uint `gorm:"not null" json:"total_price"`
 }
