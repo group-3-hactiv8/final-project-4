58f3641954fe7f682dbd058a4165999d02006cd3
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-06-03 00:02:02 +0700

    TxHistory must have rel with product n user

--- models/transaction_history.go
+++ models/transaction_history.go
@@ -9,9 +9,9 @@ import (
 
 type TransactionHistory struct {
 	gorm.Model
-	ProductId  uint `json:"product_id"`
+	ProductId  uint `gorm:"not null" json:"product_id"`
 	Product    Product
-	UserId     uint `json:"user_id"`
+	UserId     uint `gorm:"not null" json:"user_id"`
 	User       User
 	Quantity   uint `gorm:"not null" json:"quantity"`
 	TotalPrice uint `gorm:"not null" json:"total_price"`
