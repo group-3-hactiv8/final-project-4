a9689df9a030c50687da7d5715a0968cd3f7c784
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-06-18 20:04:47 +0700

    change db config

--- database/postgres.go
+++ database/postgres.go
@@ -13,10 +13,8 @@ const (
 	host     = "localhost"
 	user     = "postgres"
 	password = "postgres"
-	// password = "santoso" // password database local rakha
-	dbPort = 5432
-	// dbPort = 5433 // port db local rakha
-	dbname = "final-project-4"
+	dbPort   = 5432
+	dbname   = "final-project-4"
 )
 
 var (
