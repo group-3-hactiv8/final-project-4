7c76bbd73c4e08cb0cbabb91179dcf94812a2956
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-07-25 20:28:34 +0700

    create admin authorization

--- middlewares/authorization.go
+++ middlewares/authorization.go
@@ -2,23 +2,21 @@ package middlewares
 
 import (
 	"final-project-4/database"
-	"final-project-4/models"
 	"final-project-4/repositories/user_repository/user_pg"
 	"net/http"
-	"strconv"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-func ProductAuthorization() gin.HandlerFunc {
+func AdminAuthorization() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		db := database.GetPostgresInstance()
 		userData := c.MustGet("userData").(jwt.MapClaims)
 		userId := uint(userData["id"].(float64))
 
 		userRepo := user_pg.NewUserPG(db)
-		initialUser,err := userRepo.GetUserByID(userId)
+		initialUser, err := userRepo.GetUserByID(userId)
 		if err != nil {
 			// Handle error ketika mendapatkan user
 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
@@ -37,81 +35,6 @@ func ProductAuthorization() gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
 				"error":   "Unauthorized",
 				"message": "You are not allowed to access this product feature",
-			})
-			return
-		}
-
-		c.Next()
-	}
-}
-
-
-
-func CategoryAuthorization() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		db := database.GetPostgresInstance()
-		userData := c.MustGet("userData").(jwt.MapClaims)
-		userId := uint(userData["id"].(float64))
-
-		userRepo := user_pg.NewUserPG(db)
-		initialUser, err := userRepo.GetUserByID(userId)
-		if err != nil {
-			// Handle error ketika mendapatkan user
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-				"error":   "Internal Server Error",
-				"message": "Failed to retrieve user data",
-			})
-			return
-		}
-
-		// Cek role user, jika bukan "admin", berikan response Forbidden
-		if initialUser.Role != "admin" {
-			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
-				"error":   "Unauthorized",
-				"message": "You are not allowed to access this category feature",
-			})
-			return
-		}
-
-		c.Next()
-	}
-}
-
-
-func TaskAuthorization() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		db := database.GetPostgresInstance()
-		// misal ada user yg mau akses task berdasarkan taskId
-		// nya melalui param. Param = path variable
-		taskId, err := strconv.Atoi(c.Param("taskId"))
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error":   "Bad Request",
-				"message": "Invalid parameter (gada taskId)",
-			})
-			return
-		}
-		userData := c.MustGet("userData").(jwt.MapClaims)
-		userId := uint(userData["id"].(float64))
-		task := models.TransactionHistory{}
-
-		// cek product yg dicari berdasarkan product id nya ada atau engga
-		err = db.Select("user_id").First(&task, uint(taskId)).Error
-
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
-				"error":   "Not Found",
-				"message": "Product not found",
-			})
-			return
-		}
-
-		// task nya ada, tp cek dulu userId nya sama dengan
-		// userId si user yg lg login ngga?
-		if task.UserId != userId {
-			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
-				"error":   "Unauthorized",
-				"message": "You are not allowed to access this task",
 			})
 			return
 		}
